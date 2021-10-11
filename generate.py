890f11eaeff8f4d32df1829545a4b204eb0ec5ba
Pratyush Kumar <pratyushkumartheking@gmail.com>
2021-10-11 12:57:40 +0530

    Added feature for toggling B&W and RGB output

--- generate.py
+++ generate.py
@@ -7,11 +7,12 @@ import cv2
 ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 MAX_PIXEL_VALUE = 255
 
-def set_brightness(r, g, b, option):
+def set_brightness(pixel, option):
     """Set the measure of brightness to be used depending upon the
     option chosen, we chose between three measures namely luminance,
     lightness and average pixel values
     """
+    r, g, b = pixel
     brightness = 0
     if option == 1:
         # average of RGB as brightness
@@ -29,102 +30,49 @@ def print_matrix(ascii_matrix):
         line_extended = [p + p + p for p in line]
         print("".join(line_extended))
 
-def rgbx_approx(red, green, blue, x):
-    threshold = (x + 1) * 255 / 3
-    r = 1 if red > threshold else 0
-    g = 1 if green > threshold else 0
-    b = 1 if blue > threshold else 0
-    return (r, g, b)
-
-def rgbi_to_rgb24(r, g, b, i):
-    red = (2 * r + i) * 255 / 3
-    green = (2 * g + i) * 255 / 3
-    blue = (2 * b + i) * 255 / 3
-    return (red, green, blue)
-
-def color_distance(red_a, green_a, blue_a, red_b, green_b, blue_b):
-    d_red = red_a - red_b
-    d_green = green_a - green_b
-    d_blue = blue_a - blue_b
-    return (d_red * d_red) + (d_green * d_green) + (d_blue * d_blue)
-
-def rgbi_approx(red, green, blue):
-    r0, g0, b0 = rgbx_approx(red, green, blue, 0)
-    r1, g1, b1 = rgbx_approx(red, green, blue, 1)
-
-    red0, green0, blue0 = rgbi_to_rgb24(r0, g0, b0, 0)
-    red1, green1, blue1 = rgbi_to_rgb24(r1, g1, b1, 1)
-
-    d0 = color_distance(red, green, blue, red0, green0, blue0)
-    d1 = color_distance(red, green, blue, red1, green1, blue1)
-
-    if d0 <= d1:
-        return (r0, g0, b0, 0)
-    return (r1, g1, b1, 1)
-
-def find_closest_color(pixel):
+def find_color_rgb24(pixel):
     red, green, blue = pixel
-    r, g, b, i = rgbi_approx(red, green, blue)
-    if i == 1:
-        # increase the values by one, to signal the dither algo to play more effect on the surrounding areas
-        r = 2 if r == 1 else 0
-        g = 2 if g == 1 else 0
-        b = 2 if b == 1 else 0
-    return (r, g, b)
+    return "\033[38;2;" + str(red) + ";" + str(green) + ";" + str(blue) + "m"
 
-def convert_color_to_code(r, g, b):
-    if r + g + b == 3:
-        return "\033[37m"
-    if r + g + b == 6:
-        return "\033[97m"
-    if r + g + b == 0:
-        return "\033[30m"
-    if r == 1:
-        if g == 1:
-            return "\033[33m"
-        if b == 1:
-            return "\033[35m"
-        return "\033[31m"
-    if g == 1:
-        if b == 1:
-            return "\033[36m"
-        return "\033[32m"
-    if b == 1:
-        return "\033[34m"
-    if r == 2:
-        if g == 2:
-            return "\033[93m"
-        if b == 2:
-            return "\033[95m"
-        return "\033[91m"
-    if g == 2:
-        if b == 2:
-            return "\033[96m"
-        return "\033[92m"
-    return "\033[94m"
+# def find_color_rgb(r, g, b):
+#     if r + g + b == 765:
+#         return "\033[37m"
+#     if r + g + b == 0:
+#         return "\033[30m"
+#     if r == 255:
+#         if g == 255:
+#             return "\033[33m"
+#         if b == 255:
+#             return "\033[35m"
+#         return "\033[31m"
+#     if g == 255:
+#         if b == 255:
+#             return "\033[36m"
+#         return "\033[32m"
+#     return "\033[34m"
 
-def dither(pixels, d1, d2):
-    """Floyd-Steinberg Dithering implementation
-    to correctly encode the RGB data to B&W and set pixel data
-    """
-    output = pixels
+# def dither(colour_matrix, d1, d2):
+#     """Floyd-Steinberg Dithering implementation
+#     to correctly encode the RGB data to B&W and set pixel data
+#     """
 
-    for y in range(1, d2 - 1):
-        for x in range(1, d1 - 1):
-            oldpix = output[y, x]
-            newpix = find_closest_color(oldpix)
-            error = (0, 0, 0)
-            error = tuple(np.subtract(oldpix, newpix))
-            output[y, x] = newpix
+#     output = colour_matrix
 
-            output[y    , x + 1] = tuple([np.add(output[y    , x + 1], tuple(int(x * 7/16) for x in error))])
-            output[y + 1, x - 1] = tuple([np.add(output[y + 1, x - 1], tuple(int(x * 3/16) for x in error))])
-            output[y + 1, x    ] = tuple([np.add(output[y + 1, x    ], tuple(int(x * 5/16) for x in error))])
-            output[y + 1, x + 1] = tuple([np.add(output[y + 1, x + 1], tuple(int(x * 1/16) for x in error))])
+#     for y in range(1, d2 - 1):
+#         for x in range(1, d1 - 1):
+#             oldpix = output[y][x]
+#             newpix = round(oldpix/255) * 255
+#             error = oldpix - newpix
+#             output[y][x] = newpix
 
-    return (output)
+#             output[y    ][x + 1] += (7/16.0 * error)
+#             output[y + 1][x - 1] += (3/16.0 * error)
+#             output[y + 1][x    ] += (5/16.0 * error)
+#             output[y + 1][x + 1] += (1/16.0 * error)
 
-def print_from_image(filename):
+#     return (output)
+
+def print_from_image(filename, option):
     """Taking in an image, use its RGB values to decide upon an ASCII character
     to represent it. This ASCII character will be based upon the brightness
     measure calculated
@@ -144,43 +92,70 @@ def print_from_image(filename):
             small_im = im.resize((d1, d2))
             pixels = small_im.load()
 
-            # pixels = dither(pixels, d1, d2)
+            # if option == 0:
+            #     # 4-bit color terminal
+            #     blue_matrix = []
+            #     green_matrix = []
+            #     red_matrix = []
+
+            #     for i in range(d2):
+            #         blue_row = []
+            #         green_row = []
+            #         red_row = []
+            #         for j in range(d1):
+            #             r, g, b = pixels[j, i]
+            #             blue_row.append(b)
+            #             green_row.append(g)
+            #             red_row.append(r)
+
+            #         blue_matrix.append(blue_row)
+            #         red_matrix.append(red_row)
+            #         green_matrix.append(green_row)
+
+            #     blue_matrix = dither(blue_matrix, d1, d2)
+            #     green_matrix = dither(green_matrix, d1, d2)
+            #     red_matrix = dither(red_matrix, d1, d2)
 
             ascii_matrix = []
             for i in range(d2):
                 ascii_row = []
                 for j in range(d1):
-                    r, g, b = pixels[j, i]
-                    brightness = set_brightness(r, g, b, 2)
+                    brightness = set_brightness(pixels[j, i], 3)
                     symbol_index = int((brightness / MAX_PIXEL_VALUE) * len(ASCII_CHARS)) - 1
-                    r, g, b = find_closest_color(pixels[j, i])
-                    ascii_row.append(convert_color_to_code(r, g, b))
+                    if option == 1:
+                        color = find_color_rgb24(pixels[j, i])
+                        ascii_row.append(color)
                     ascii_row.append(ASCII_CHARS[symbol_index])
                 ascii_matrix.append(ascii_row)
 
+            print("\033[40m\033[37m", end='')
             print_matrix(ascii_matrix)
+            print("\033[0m", end='')
     except OSError: 
         print("Could not open image file!")
 
-def read_media(infile):
+def read_media(infile, option):
     vidcap = cv2.VideoCapture(infile)
     i = 0
+    # control frame rate in image
     frame_skip = 0
     while vidcap.isOpened():
+        # read frames from the image
         success, image = vidcap.read()
         if not success:
             break
         if i > frame_skip - 1:
-            # image = cv2.convertScaleAbs(image, alpha=1.25, beta=60)
+            # enhance the image for terminal display
+            if option == 1:
+                image = cv2.convertScaleAbs(image, alpha=1.5, beta=60)
             cv2.imwrite("frame.jpg", image)
             i = 0
-            print("\033[40m", end='')
-            print_from_image("frame.jpg")
-            print("\033[0m", end='')
+            print_from_image("frame.jpg", option)
             continue
         i += 1
     vidcap.release()
     cv2.destroyAllWindows()
 
-infile = sys.argv[1];
-read_media(infile)
+infile = sys.argv[1]
+colored_output = sys.argv[2]
+read_media(infile, colored_output)
