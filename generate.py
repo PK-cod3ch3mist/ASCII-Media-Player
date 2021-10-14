f1df7df9e897c697352ede661c5754a858411cfe
Pratyush Kumar <pratyushkumartheking@gmail.com>
2021-10-14 16:23:33 +0530

    Modularized code

--- generate.py
+++ generate.py
@@ -7,23 +7,20 @@ import cv2
 ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 MAX_PIXEL_VALUE = 255
 
-def set_brightness(pixel, option):
-    """Set the measure of brightness to be used depending upon the
-    option chosen, we chose between three measures namely luminance,
-    lightness and average pixel values
-    """
-    r, g, b = pixel
-    brightness = 0
-    if option == 1:
-        # average of RGB as brightness
-        brightness = (r + g + b) / 3
-    elif option == 2:
-        # lightness of pixel as brightness
-        brightness = (max(r, g, b) + min(r, g, b)) / 2
-    elif option == 3:
-        # luminance being square root of weighted RGB values (weighted according to the perception of the human eye)
-        brightness = (0.299 * r + 0.587 * g + 0.114 * b)
-    return brightness
+def get_pixel_matrix(image):
+    image = image.convert("RGB")
+            
+    # current row and column size definitions
+    ac_row, ac_col = image.size
+    # d1 and d2 are the width and height of image resp
+    size = os.get_terminal_size()
+    d2 = min(size.lines - 1, int((ac_col * size.columns) / ac_row))
+    d1 = min(int(size.columns / 3), int((ac_row * d2) / ac_col))
+
+    # set image to determined d1 and column size
+    im = image.resize((d1, d2))
+    pixels = list(im.getdata())
+    return [pixels[i:i+im.width] for i in range(0, len(pixels), im.width)]
 
 def print_matrix(ascii_matrix):
     print("\033[1;1H", end='')
@@ -31,47 +28,50 @@ def print_matrix(ascii_matrix):
         line_extended = [p + p + p for p in line]
         print("".join(line_extended))
 
-def find_color_rgb24(pixel):
-    red, green, blue = pixel
-    return "\033[38;2;" + str(red) + ";" + str(green) + ";" + str(blue) + "m"
+def get_color_matrix(pixels):
+    color_matrix = []
+    for row in pixels:
+        color_matrix_row = []
+        for p in row:
+            color_matrix_row.append("\033[38;2;" + str(p[0]) + ";" + str(p[1]) + ";" + str(p[2]) + "m")
+        color_matrix.append(color_matrix_row)
+    return color_matrix
 
-# def find_color_rgb(r, g, b):
-#     if r + g + b == 765:
-#         return "\033[37m"
-#     if r + g + b == 0:
-#         return "\033[30m"
-#     if r == 255:
-#         if g == 255:
-#             return "\033[33m"
-#         if b == 255:
-#             return "\033[35m"
-#         return "\033[31m"
-#     if g == 255:
-#         if b == 255:
-#             return "\033[36m"
-#         return "\033[32m"
-#     return "\033[34m"
+def get_intensity_matrix(pixels, option):
+    """Set the measure of brightness to be used depending upon the
+    option chosen, we chose between three measures namely luminance,
+    lightness and average pixel values
+    """
+    intensity_matrix = []
+    for row in pixels:
+        intensity_matrix_row = []
+        for p in row:
+            intensity = 0
+            if option == 1:
+                intensity = ((p[0] + p[1] + p[2]) / 3.0)
+            elif option == 2:
+                intensity = (max(p[0], p[1], p[2]) + min(p[0], p[1], p[2])) / 2
+            elif option == 3:
+                intensity = (0.299 * p[0] * p[0] + 0.587 * p[1] * p[1] + 0.114 * p[2] * p[2]) ** 0.5
+            else:
+                raise Exception("Unrecognised intensity option: %d" % option)
+            intensity_matrix_row.append(intensity)
+        intensity_matrix.append(intensity_matrix_row)
 
-# def dither(colour_matrix, d1, d2):
-#     """Floyd-Steinberg Dithering implementation
-#     to correctly encode the RGB data to B&W and set pixel data
-#     """
+    return intensity_matrix
 
-#     output = colour_matrix
+def normalize_intensity_matrix(intensity_matrix):
+    normalized_intensity_matrix = []
+    max_pixel = max(map(max, intensity_matrix))
+    min_pixel = min(map(min, intensity_matrix))
+    for row in intensity_matrix:
+        rescaled_row = []
+        for p in row:
+            r = MAX_PIXEL_VALUE * (p - min_pixel) / float(max_pixel - min_pixel)
+            rescaled_row.append(r)
+        normalized_intensity_matrix.append(rescaled_row)
 
-#     for y in range(1, d2 - 1):
-#         for x in range(1, d1 - 1):
-#             oldpix = output[y][x]
-#             newpix = round(oldpix/255) * 255
-#             error = oldpix - newpix
-#             output[y][x] = newpix
-
-#             output[y    ][x + 1] += (7/16.0 * error)
-#             output[y + 1][x - 1] += (3/16.0 * error)
-#             output[y + 1][x    ] += (5/16.0 * error)
-#             output[y + 1][x + 1] += (1/16.0 * error)
-
-#     return (output)
+    return normalized_intensity_matrix
 
 def print_from_image(filename, option):
     """Taking in an image, use its RGB values to decide upon an ASCII character
@@ -79,53 +79,21 @@ def print_from_image(filename, option):
     measure calculated
     """
     try:
-        with Image.open(filename) as im:
-            im = im.convert("RGB")
-            
-            # current row and column size definitions
-            # TODO: make this dynamic to set according to terminals current size
-            ac_row, ac_col = im.size
-            # d1 and d2 are the width and height of image resp
-            size = os.get_terminal_size()
-            d2 = min(size.lines - 1, int((ac_col * size.columns) / ac_row))
-            d1 = min(int(size.columns / 3), int((ac_row * d2) / ac_col))
-
-            # set image to determined d1 and column size
-            small_im = im.resize((d1, d2))
-            pixels = small_im.load()
-
-            # if option == 0:
-            #     # 4-bit color terminal
-            #     blue_matrix = []
-            #     green_matrix = []
-            #     red_matrix = []
-
-            #     for i in range(d2):
-            #         blue_row = []
-            #         green_row = []
-            #         red_row = []
-            #         for j in range(d1):
-            #             r, g, b = pixels[j, i]
-            #             blue_row.append(b)
-            #             green_row.append(g)
-            #             red_row.append(r)
-
-            #         blue_matrix.append(blue_row)
-            #         red_matrix.append(red_row)
-            #         green_matrix.append(green_row)
-
-            #     blue_matrix = dither(blue_matrix, d1, d2)
-            #     green_matrix = dither(green_matrix, d1, d2)
-            #     red_matrix = dither(red_matrix, d1, d2)
+        with Image.open(filename) as image:
+            pixels = get_pixel_matrix(image)
+            intensity_matrix = get_intensity_matrix(pixels, 3)
+            intensity_matrix = normalize_intensity_matrix(intensity_matrix)
+            color_matrix = get_color_matrix(pixels)
 
             ascii_matrix = []
-            for i in range(d2):
+            for i in range(len(intensity_matrix)):
                 ascii_row = []
-                for j in range(d1):
-                    brightness = set_brightness(pixels[j, i], 3)
-                    symbol_index = int((brightness / MAX_PIXEL_VALUE) * len(ASCII_CHARS)) - 1
+                for j in range(len(intensity_matrix[0])):
+                    intensity = intensity_matrix[i][j]
+                    symbol_index = int(intensity / MAX_PIXEL_VALUE * len(ASCII_CHARS)) - 1
+                    symbol_index = symbol_index + 1 if symbol_index < 0 else symbol_index
                     if option == 1:
-                        color = find_color_rgb24(pixels[j, i])
+                        color = color_matrix[i][j]
                         ascii_row.append(color)
                     ascii_row.append(ASCII_CHARS[symbol_index])
                 ascii_matrix.append(ascii_row)
@@ -148,7 +116,8 @@ def read_media(infile, option):
         if not success:
             break
         if i > frame_skip - 1:
-            # enhance the image for terminal display
+            # enhance the image (increase contrast and brightness) for terminal display
+            # TURN OFF (by commenting) IF YOU PREFER THE ORIGINAL COLOURS
             if option == 1:
                 image = cv2.convertScaleAbs(image, alpha=1.25, beta=50)
             cv2.imwrite("frame.jpg", image)
