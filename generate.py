f9cf70ab1ab17aa57067f3f280916249dbb4de6c
Pratyush Kumar <pratyushkumartheking@gmail.com>
2022-05-27 22:15:02 +0530

    Reformatted code

--- generate.py
+++ generate.py
@@ -6,14 +6,21 @@ import cv2
 import pysrt
 import curses
 
-ASCII_CHAR_ARRAY = (" .:-=+*#%@", " .,:ilwW", " ▏▁░▂▖▃▍▐▒▀▞▚▌▅▆▊▓▇▉█", " `^|1aUBN", " .`!?xyWN")
+ASCII_CHAR_ARRAY = (
+    " .:-=+*#%@",
+    " .,:ilwW",
+    " ▏▁░▂▖▃▍▐▒▀▞▚▌▅▆▊▓▇▉█",
+    " `^|1aUBN",
+    " .`!?xyWN",
+)
 
 ASCII_CHARS = ASCII_CHAR_ARRAY[3]
 MAX_PIXEL_VALUE = 255
 
+
 def vid_render(st_matrix, st, ed, option):
     media.addstr(0, 1, "Video Playback")
-    pixels = [st_matrix[i][:] for i in range (st, ed)]
+    pixels = [st_matrix[i][:] for i in range(st, ed)]
     # CONFIG OPTION - intensity measure
     intensity_matrix = get_intensity_matrix(pixels, 3)
     intensity_matrix = normalize_intensity_matrix(intensity_matrix)
@@ -25,7 +32,11 @@ def vid_render(st_matrix, st, ed, option):
             intensity = intensity_matrix[i][j]
             symbol_index = int(intensity / MAX_PIXEL_VALUE * len(ASCII_CHARS)) - 1
             symbol_index = symbol_index + 1 if symbol_index < 0 else symbol_index
-            asciiStr = ASCII_CHARS[symbol_index] + ASCII_CHARS[symbol_index] + ASCII_CHARS[symbol_index]
+            asciiStr = (
+                ASCII_CHARS[symbol_index]
+                + ASCII_CHARS[symbol_index]
+                + ASCII_CHARS[symbol_index]
+            )
             if option == 1:
                 color = color_matrix[i][j]
                 media.addstr(i + 1, offset, asciiStr, curses.color_pair(color))
@@ -35,9 +46,13 @@ def vid_render(st_matrix, st, ed, option):
 
     media.refresh()
 
+
 def subtitle_show(subs, tstamp_ms):
     """Function to get subtitles of current frame and display them"""
-    parts = subs.slice(starts_before={'milliseconds': int(tstamp_ms)}, ends_after={'milliseconds': int(tstamp_ms)})
+    parts = subs.slice(
+        starts_before={"milliseconds": int(tstamp_ms)},
+        ends_after={"milliseconds": int(tstamp_ms)},
+    )
     captions.addstr(0, 1, "Captions")
     captions.move(1, 1)
     for part in parts:
@@ -45,10 +60,11 @@ def subtitle_show(subs, tstamp_ms):
 
     captions.refresh()
 
+
 def get_pixel_matrix(image):
     """Function to get image from the media file and change its dimensions to fit the terminal, then turning it into pixel matrix."""
     image = image.convert("RGB")
-            
+
     # current row and column size definitions
     ac_row, ac_col = image.size
     # d1 and d2 are the width and height of image resp
@@ -59,7 +75,8 @@ def get_pixel_matrix(image):
     # set image to determined d1 and column size
     im = image.resize((d1, d2))
     pixels = list(im.getdata())
-    return [pixels[i:i+im.width] for i in range(0, len(pixels), im.width)]
+    return [pixels[i : i + im.width] for i in range(0, len(pixels), im.width)]
+
 
 def get_color_matrix(pixels):
     """Function to get the colour codes (ANSI escape sequences) from RGB values of pixel."""
@@ -67,9 +84,9 @@ def get_color_matrix(pixels):
     for row in pixels:
         color_matrix_row = []
         for p in row:
-            r = round(p[0]/255)
-            g = round(p[1]/255)
-            b = round(p[2]/255)
+            r = round(p[0] / 255)
+            g = round(p[1] / 255)
+            b = round(p[2] / 255)
             cNum = 0
             if r + g + b != 3:
                 if r == 1 and g == 1:
@@ -88,6 +105,7 @@ def get_color_matrix(pixels):
         color_matrix.append(color_matrix_row)
     return color_matrix
 
+
 def get_intensity_matrix(pixels, option):
     """Function to set the measure of brightness to be used depending upon the
     option, choose between three measures namely luminance,
@@ -99,17 +117,20 @@ def get_intensity_matrix(pixels, option):
         for p in row:
             intensity = 0
             if option == 1:
-                intensity = ((p[0] + p[1] + p[2]) / 3.0)
+                intensity = (p[0] + p[1] + p[2]) / 3.0
             elif option == 2:
                 intensity = (max(p[0], p[1], p[2]) + min(p[0], p[1], p[2])) / 2
             elif option == 3:
-                intensity = (0.299 * p[0] * p[0] + 0.587 * p[1] * p[1] + 0.114 * p[2] * p[2]) ** 0.5
+                intensity = (
+                    0.299 * p[0] * p[0] + 0.587 * p[1] * p[1] + 0.114 * p[2] * p[2]
+                ) ** 0.5
             else:
                 raise Exception("Unrecognised intensity option: %d" % option)
             intensity_matrix_row.append(intensity)
         intensity_matrix.append(intensity_matrix_row)
 
     return intensity_matrix
+
 
 def normalize_intensity_matrix(intensity_matrix):
     """Function to normalize the intensity matrix so that values fall between acceptable limits."""
@@ -128,6 +149,7 @@ def normalize_intensity_matrix(intensity_matrix):
 
     return normalized_intensity_matrix
 
+
 def print_from_image(filename, option):
     """Function to take in an image & use its RGB values to decide upon an ASCII character
     to represent it. This ASCII character will be based upon the brightness
@@ -139,8 +161,9 @@ def print_from_image(filename, option):
         with Image.open(filename) as image:
             pixels = get_pixel_matrix(image)
             vid_render(pixels, 0, len(pixels), option)
-    except OSError: 
+    except OSError:
         print("Could not open image file!")
+
 
 def read_media_sub(vidfile, subfile, option):
     """Function to read the media file and pass on data to rendering functions frame by frame."""
@@ -156,12 +179,13 @@ def read_media_sub(vidfile, subfile, option):
         cv2.imwrite("./data/frame.jpg", image)
         print_from_image("./data/frame.jpg", option)
         subtitle_show(subs, vidcap.get(cv2.CAP_PROP_POS_MSEC))
-        dur = (time.process_time() - dur)
+        dur = time.process_time() - dur
         dur *= 1000
-        if (round(1000/fps - dur) >= 0):
-            curses.napms(round(1000/fps - dur))
+        if round(1000 / fps - dur) >= 0:
+            curses.napms(round(1000 / fps - dur))
     vidcap.release()
     cv2.destroyAllWindows()
+
 
 def read_media(vidfile, option):
     """Function to read the media file and pass on data to rendering functions frame by frame."""
@@ -175,12 +199,13 @@ def read_media(vidfile, option):
         dur = time.process_time()
         cv2.imwrite("./data/frame.jpg", image)
         print_from_image("./data/frame.jpg", option)
-        dur = (time.process_time() - dur)
+        dur = time.process_time() - dur
         dur *= 1000
-        if (round(1000/fps - dur) >= 0):
-            curses.napms(round(1000/fps - dur))
+        if round(1000 / fps - dur) >= 0:
+            curses.napms(round(1000 / fps - dur))
     vidcap.release()
     cv2.destroyAllWindows()
+
 
 stdscr = curses.initscr()
 curses.noecho()
@@ -193,20 +218,26 @@ curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
 curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
 curses.init_pair(6, curses.COLOR_BLUE, curses.COLOR_BLACK)
 if len(sys.argv) == 3:
-    beginX = 0; beginY = 0
-    height = curses.LINES; width = curses.COLS
+    beginX = 0
+    beginY = 0
+    height = curses.LINES
+    width = curses.COLS
     media = curses.newwin(height - 1, width - 1, beginY, beginX)
     media.border(0, 0, 0, 0, 0, 0, 0, 0)
     vidfile = sys.argv[1]
     colored_output = int(sys.argv[2])
     read_media(vidfile, colored_output)
 else:
-    beginX = 0; beginY = 0
-    height = curses.LINES - 5; width = curses.COLS
+    beginX = 0
+    beginY = 0
+    height = curses.LINES - 5
+    width = curses.COLS
     media = curses.newwin(height, width, beginY, beginX)
     media.border(0, 0, 0, 0, 0, 0, 0, 0)
-    beginX = 0; beginY = curses.LINES - 5
-    height = 5; width = curses.COLS
+    beginX = 0
+    beginY = curses.LINES - 5
+    height = 5
+    width = curses.COLS
     captions = curses.newwin(height, width, beginY, beginX)
     captions.border(0, 0, 0, 0, 0, 0, 0, 0)
     vidfile = sys.argv[1]
